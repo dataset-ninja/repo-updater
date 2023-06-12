50eab28ba945febfeacea2c854afbd2e4cf86dbe
Stan Soldatov <118521851+iwatkot@users.noreply.github.com>
2023-06-12 12:29:14 +0300

    Added pip install for requirements.txt.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import os
 import json
+import os
 import shutil
 import subprocess
-
 from typing import Dict
 
-from git import Repo
 import supervisely as sly
+from git import Repo
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 REPO_DIR = os.path.join(CURRENT_DIR, "repos")
@@ -28,6 +27,35 @@ def process_repo(repo: Dict):
     repo = Repo.clone_from(repo_url, local_repo_path)
 
     sly.logger.info(f"Cloned repo {repo_name} to {local_repo_path}.")
+
+    repo_requirements_path = os.path.join(local_repo_path, "requirements.txt")
+    # Installing repo requirements to local environment, if requirements.txt exists.
+    if os.path.exists(repo_requirements_path):
+        sly.logger.info(f"Found requirements.txt in {repo_name} repo, will install requirements.")
+        # Excluding supervisely and dataset-tools from requirements.txt
+        repo_requirements = open(repo_requirements_path, "r").readlines()
+        to_install = []
+        for line in repo_requirements:
+            if "supervisely" not in line and "dataset-tools" not in line:
+                to_install.append(line.strip())
+
+        sly.logger.info(f"Found {len(to_install)} requirements to install.")
+        # Installing requirements.
+        for line in to_install:
+            sly.logger.info(f"Installing {line}...")
+            return_code = subprocess.check_call(
+                f"pip install {line}",
+                shell=True,
+                cwd=local_repo_path,
+                stdout=subprocess.PIPE,
+                text=True,
+            )
+
+            if return_code != 0:
+                sly.logger.error(f"Failed to install {line}.")
+                raise RuntimeError(f"Failed to install {line}.")
+
+            sly.logger.info(f"Successfully installed {line}.")
 
     script_path = os.path.join(local_repo_path, "src", "main.py")
     command = f"PYTHONPATH=\"{local_repo_path}:${{PYTHONPATH}}\" python {script_path} --forces '{json.dumps(forces)}'"
@@ -58,9 +86,7 @@ def process_repo(repo: Dict):
 
     # If there is no changes in index, then there is nothing to commit.
     if not index.diff("HEAD"):
-        sly.logger.info(
-            f"No files was added to index in {repo_name} repo. Nothing to commit."
-        )
+        sly.logger.info(f"No files was added to index in {repo_name} repo. Nothing to commit.")
         sly.fs.remove_dir(local_repo_path)
         return
 
